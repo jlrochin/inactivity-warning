c35dfe3c5fac371f4fcbe17c2959ceea47c40274
jlru <jlrochin@users.noreply.github.com>
2025-09-04 21:51:26 -0600

    fix: format Vue template to comply with ESLint rules

--- src/components/InactivityWarning.vue
+++ src/components/InactivityWarning.vue
@@ -4,13 +4,30 @@
     class="vue-inactivity-warning-overlay"
     :class="overlayClass"
   >
-    <div class="vue-inactivity-warning-modal" :class="modalClass">
+    <div 
+      class="vue-inactivity-warning-modal" 
+      :class="modalClass"
+    >
       <!-- Header -->
       <div class="vue-inactivity-warning-header">
-        <div class="vue-inactivity-warning-icon" :class="iconClass">
+        <div 
+          class="vue-inactivity-warning-icon" 
+          :class="iconClass"
+        >
           <slot name="icon">
-            <svg class="vue-inactivity-warning-icon-svg" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+            <svg 
+              class="vue-inactivity-warning-icon-svg" 
+              fill="none" 
+              stroke="currentColor" 
+              viewBox="0 0 24 24" 
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path 
+                stroke-linecap="round" 
+                stroke-linejoin="round" 
+                stroke-width="2" 
+                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+              />
             </svg>
           </slot>
         </div>
