c62ea5d632fe2f0c303e2319f5ea34c1f8a0b863
jlru <jlrochin@users.noreply.github.com>
2025-09-04 21:46:06 -0600

    fix: add ESLint configuration and missing dependencies

---
+++ .eslintrc.js
@@ -0,0 +1,44 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    'eslint:recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'no-unused-vars': 'warn',
+    'no-console': 'off',
+    'prefer-const': 'error',
+    'no-var': 'error',
+  },
+  overrides: [
+    {
+      files: ['*.vue'],
+      extends: [
+        'plugin:vue/vue3-recommended',
+      ],
+      parser: 'vue-eslint-parser',
+      parserOptions: {
+        parser: '@babel/eslint-parser',
+        requireConfigFile: false,
+      },
+    },
+    {
+      files: ['*.ts', '*.tsx'],
+      extends: [
+        '@typescript-eslint/recommended',
+      ],
+      parser: '@typescript-eslint/parser',
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+  ],
+};
