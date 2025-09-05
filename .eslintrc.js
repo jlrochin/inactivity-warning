ed34355f2807cf2dd221d521f5ccf0e1bfbba997
jlru <jlrochin@users.noreply.github.com>
2025-09-04 22:05:04 -0600

    fix: add TypeScript support to ESLint configuration

--- .eslintrc.js
+++ .eslintrc.js
@@ -29,5 +29,21 @@ module.exports = {
         requireConfigFile: false,
       },
     },
+    {
+      files: ['*.ts', '*.tsx'],
+      parser: '@typescript-eslint/parser',
+      extends: [
+        'eslint:recommended',
+        '@typescript-eslint/recommended',
+      ],
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+      rules: {
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-explicit-any': 'off',
+      },
+    },
   ],
 };
